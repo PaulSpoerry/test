5d0743d7d3ef10e5b097268f2badc70ad0d1a059
Paul Spoerry <pspoerry@DoctorsSupplementStore.com>
2022-08-10 18:55:17 -0500

    test

---
+++ conf.js
@@ -0,0 +1,16 @@
+const Conf = require('conf');
+
+const config = new Conf();
+
+config.set('unicorn', '🦄');
+console.log(config.get('unicorn'));
+//=> '🦄'
+
+// Use dot-notation to access nested properties
+config.set('foo.bar', true);
+console.log(config.get('foo'));
+//=> {bar: true}
+
+config.delete('unicorn');
+console.log(config.get('unicorn'));
+//=> undefined
